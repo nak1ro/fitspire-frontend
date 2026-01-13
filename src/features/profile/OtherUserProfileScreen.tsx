682a6af3ad448e3d20e774d69e5009a294871516
Maxim Azarpv <max6808025@gmail.com>
2026-01-13 23:57:11 +0100

    chore: remove old theme and ThemeProvider components

--- src/features/profile/OtherUserProfileScreen.tsx
+++ src/features/profile/OtherUserProfileScreen.tsx
@@ -3,18 +3,18 @@ import { ScrollView, StyleSheet } from 'react-native';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { MainStackParamList } from '../../navigation/MainStack';
 import OtherUserProfileCard from './OtherUserProfileCard';
-import { useTheme } from '../../ui/theme/ThemeProvider';
+import { useTheme } from '../../common/hooks/useTheme';
 import { log } from '../../utils/logger';
 
 type RouteProps = RouteProp<MainStackParamList, 'OtherUserProfile'>;
 
 export default function OtherUserProfileScreen() {
   const { params } = useRoute<RouteProps>();
-  const { theme } = useTheme();
+  const theme = useTheme();
 
   return (
     <ScrollView
-      style={{ backgroundColor: theme.colors.bg }}
+      style={{ backgroundColor: theme.colors.background }}
       contentContainerStyle={styles.container}
     >
       <OtherUserProfileCard
